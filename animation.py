235df3df1ffe7ed5a84e5a8e536b7a474d462f0a
Shamsi567 <130103295+Shamsi567@users.noreply.github.com>
2023-04-08 02:08:32 -0700

    Add files via upload
    Python Turtle Graphics

---
+++ animation.py
@@ -0,0 +1,12 @@
+from turtle import*
+bgcolor('black')
+speed(0)
+hideturtle()
+for i in range(120):
+    color('yellow')
+    circle(i)
+    color('red')
+    circle(i*0.8)
+    right(3)
+    forward(3)
+done()     
